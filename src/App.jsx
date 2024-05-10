ec2fee444bced97c7cdd634cf255ea7f4c04f324
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2024-05-10 08:14:09 +0200

    Added to div - container css className

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import styles from './App.module.css'
 
 function App() {
   return (
-    <div>
+    <div className={styles.container}>
       <header>
         <h1>Do zrobienia</h1>
         <h2>5 zadań</h2>
