d81fac2d1097ae8200b232575f30e0bf84483a11
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2024-05-10 08:19:37 +0200

    Flex text and button and centerized

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import styles from './App.module.css'
 function App() {
   return (
     <div className={styles.container}>
-      <header>
-        <h1>Do zrobienia</h1>
-        <h2>5 zadań</h2>
+      <header className={styles.header}>
+        <div>
+          <h1>Do zrobienia</h1>
+          <h2>5 zadań</h2>
+        </div>
         <button>+</button>
       </header>
     </div>
