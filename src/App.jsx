6e37a6c7a60dff0cef405bfff0eb44355d4d8dd2
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2024-05-10 08:08:38 +0200

    inital html structure project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import './App.css'
 
 function App() {
-  return <div></div>
+  return (
+    <div>
+      <header>
+        <h1>Do zrobienia</h1>
+        <h2>5 zadań</h2>
+        <button>+</button>
+      </header>
+    </div>
+  )
 }
 
 export default App
