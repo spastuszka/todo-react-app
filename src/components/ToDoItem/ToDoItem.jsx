94bd7334339149de5e9c69e72f991d817fb7b921
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2024-05-11 10:59:42 +0200

    Added don class with spread operator and sipmle logic

--- src/components/ToDoItem/ToDoItem.jsx
+++ src/components/ToDoItem/ToDoItem.jsx
@@ -4,7 +4,9 @@ import styles from './ToDoItem.module.css'
 export function ToDoItem({ name, done }) {
   return (
     <li className={styles.item}>
-      <span className={styles.name}>{name}</span>
+      <span className={`${styles.name} ${done ? styles.done : ''}`}>
+        {name}
+      </span>
       {!done && <Button>Zrobione</Button>}
       <Button>Usuń</Button>
     </li>
