1e4ba4939f6f0654b06fcf7f263b490b41320346
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2024-05-11 13:12:29 +0200

    Elevated of logic to separate functions

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,33 @@ function App() {
     { name: 'Wyrzycić śmieci', done: true, id: 2 },
   ])
 
+  function addItem(newToDoName) {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      {
+        name: newToDoName,
+        done: false,
+        id: prevTodos.length > 0 ? prevTodos.at(-1).id + 1 : 0,
+      },
+    ])
+    setIsFormShown(false)
+  }
+
+  function deleteItem(id) {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id))
+  }
+
+  function finishItem(id) {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => {
+        if (todo.id !== id) {
+          return todo
+        }
+        return { ...todo, done: true }
+      })
+    )
+  }
+
   return (
     <div className={styles.container}>
       <header className={styles.header}>
@@ -30,19 +57,7 @@ function App() {
       </header>
       {isFormShown && (
         // prevTodos - funkcja aktualizująca, biorąca stary stan tablicy
-        <Form
-          onFormSubmit={(newToDoName) => {
-            setTodos((prevTodos) => [
-              ...prevTodos,
-              {
-                name: newToDoName,
-                done: false,
-                id: prevTodos.length > 0 ? prevTodos.at(-1).id + 1 : 0,
-              },
-            ])
-            setIsFormShown(false)
-          }}
-        />
+        <Form onFormSubmit={(newToDoName) => addItem(newToDoName)} />
       )}
       <ul>
         {todos.map(({ id, name, done }) => (
@@ -50,21 +65,8 @@ function App() {
             key={id}
             name={name}
             done={done}
-            onDeleteButtonClick={() => {
-              setTodos((prevTodos) =>
-                prevTodos.filter((todo) => todo.id !== id)
-              )
-            }}
-            onDoneButtonClick={() => {
-              setTodos((prevTodos) =>
-                prevTodos.map((todo) => {
-                  if (todo.id !== id) {
-                    return todo
-                  }
-                  return { ...todo, done: true }
-                })
-              )
-            }}
+            onDeleteButtonClick={() => deleteItem(id)}
+            onDoneButtonClick={() => finishItem(id)}
           />
         ))}
       </ul>
