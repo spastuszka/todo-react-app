2d76302535e70a8b4abea00decd231c186b128c4
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2024-05-11 11:47:56 +0200

    Added state about form shown

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import { useState } from 'react'
 import styles from './App.module.css'
 import { Form } from './components/Form/Form'
 import { ToDoItem } from './components/ToDoItem/ToDoItem'
 import { getSubHeading } from './utils/getSubHeading'
 
 function App() {
+  const [isFormShown, setIsFormShown] = new useState(false)
+
   const todos = [
     { name: 'Zapłacić rachunki', done: false, id: 1 },
     { name: 'Wyrzycić śmieci', done: true, id: 2 },
   ]
+
   return (
     <div className={styles.container}>
       <header className={styles.header}>
@@ -15,9 +19,16 @@ function App() {
           <h1>Do zrobienia</h1>
           <h2>{getSubHeading(todos.length)}</h2>
         </div>
-        <button className={styles.button}>+</button>
+        {!isFormShown && (
+          <button
+            onClick={() => setIsFormShown(true)}
+            className={styles.button}
+          >
+            +
+          </button>
+        )}
       </header>
-      <Form />
+      {isFormShown && <Form />}
       <ul>
         {todos.map(({ id, name, done }) => (
           <ToDoItem key={id} name={name} done={done} />
